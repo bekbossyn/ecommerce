69f05f03c8914a922c87313696a753073f2f8da3
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-02-12 03:18:05 +0600

    contact and about pages

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,7 +3,27 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Hello world !",
+        "content": "Welcome to the Home Page.",
+    }
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    context = {
+        "title": "About Page",
+        "content": "Welcome to the About Page.",
+    }
+    return render(request, "home_page.html", context)
+
+
+def contact_page(request):
+    context = {
+        "title": "Contact Page",
+        "content": "Welcome to the Contact Page.",
+    }
+    return render(request, "home_page.html", context)
 
 
 def home_page_old(request):
