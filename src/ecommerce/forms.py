a87dc26bcaaa45d69dcacf5f96e01bed1eac73a3
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-02-12 03:50:17 +0600

    Django Form-> widgets with attributes example

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -2,7 +2,28 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    fullname = forms.CharField()
-    email = forms.EmailField()
-    content = forms.CharField()
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your full name",
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your email",
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your message",
+            }
+        )
+    )
 
