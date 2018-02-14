b91115eab7d63887a9f281aba7e79ded72d9d4fc
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-02-15 01:24:46 +0600

    images with path

--- src/products/models.py
+++ src/products/models.py
@@ -1,10 +1,33 @@
+import random
+
+import os
 from django.db import models
+
+
+def get_filename_ext(file_path):
+    basename = os.path.basename(file_path)
+    name, ext = os.path.splitext(basename)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)
+    new_filename = random.randint(1, 23234234)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    # final_filename = f'{new_filename}{ext}'     # python 3.6
+    return 'products/{new_filename}/{final_filename}'.format(
+        new_filename=new_filename,
+        final_filename=final_filename
+    )
 
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
