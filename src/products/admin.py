f8e0e84ea0c33fa95cc74b3b6c486de0b5abf61e
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-02-20 11:28:58 +0600

    crete timestamp, nav bar finished with urls

--- src/products/admin.py
+++ src/products/admin.py
@@ -4,7 +4,7 @@ from .models import Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['pk', '__str__', 'slug']
+    list_display = ['pk', '__str__', 'slug', 'timestamp']
 
     class Meta:
         model = Product
