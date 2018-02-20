3efdc5b16fb2216d4569739e07d3463676d947f4
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-02-20 11:05:59 +0600

    urls in templates

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -23,11 +23,11 @@ from .views import home_page, about_page, contact_page, login_page, register_pag
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home_page),
-    url(r'^about/$', about_page),
-    url(r'^contact/$', contact_page),
-    url(r'^login/$', login_page),
-    url(r'^register/$', register_page),
+    url(r'^$', home_page, name="home"),
+    url(r'^about/$', about_page, name="about"),
+    url(r'^contact/$', contact_page, name="contact"),
+    url(r'^login/$', login_page, name="login"),
+    url(r'^register/$', register_page, name="register"),
     url(r'^products/', include("products.urls", namespace="products")),
 ]
 
