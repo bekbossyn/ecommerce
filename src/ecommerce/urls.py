2da1ed7e231d932a5a894183ded79eee6ce65497
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2018-02-26 13:51:07 +0600

    search app added

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     url(r'^login/$', login_page, name="login"),
     url(r'^register/$', register_page, name="register"),
     url(r'^products/', include("products.urls", namespace="products")),
+    url(r'^search/', include("search.urls", namespace="search")),
 
     url(r'^bootstrap/$', TemplateView.as_view(template_name="bootstrap/example.html")),
 ]
